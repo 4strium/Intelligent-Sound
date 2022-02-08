10634d62404382c8ad5450575b8db522dff3c4ef
Astrium <romain.mellaza@orange.fr>
2022-02-08 14:53:48 +0100

    Create index.html and styles.css
    Create a web page to interpret the results obtained

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
+import webbrowser
 # Code that executes the subroutines one after the other.
 
 exec(open("record.py").read())
 
 exec(open("analyze.py").read())
+
+current_dir = os.getcwd()
+
+new = 2 # open in a new tab, if possible
+url = current_dir + "/index.html"
+webbrowser.open(url,new=new)
