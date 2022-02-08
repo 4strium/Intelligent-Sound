8227ce846ef1fb5ad36e5187dbc8eec6ae9656fd
Astrium <romain.mellaza@orange.fr>
2022-02-08 11:16:55 +0100

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -1,5 +1,6 @@
 import librosa, librosa.display
 import matplotlib.pyplot as plt
+import numpy as np
 
 file = "Mon_enregistrement.wav"
 
@@ -8,4 +9,14 @@ signal, sr = librosa.load(file, sr=22050) # sr * T -> 22050 * 30
 librosa.display.waveshow(signal, sr=sr)
 plt.xlabel("Temps")                            # Nom de l'axe x
 plt.ylabel("Amplitude")                        # Nom de l'axe y
+plt.show()
+
+#fft -> spectrum :
+fft = np.fft.fft(signal)
+
+magnitude = np.abs(fft)
+frequency = np.linspace(0, sr, len(magnitude))
+plt.plot(frequency, magnitude)
+plt.xlabel("Frequence")                            # Nom de l'axe x
+plt.ylabel("Magnitude")                        # Nom de l'axe y
 plt.show()
